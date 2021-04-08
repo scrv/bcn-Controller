ed4c3c78985889850c2bb9d849c7db7512e74aa5
Sebastian Carrera <scrv.201@outlook.com>
2021-04-07 20:51:00 -0600

    WiFiManager & Firebase  libs added to the project
    Because:
    - WiFiManager handles the device connection to the internet
    - Firebase will be in charge of connecting the back-end

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,39 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h> //https://github.com/esp8266/Arduino
 
-void setup() {
-  // put your setup code here, to run once:
+//WiFiManager
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h> //https://github.com/tzapu/WiFiManager
+
+// Firebase
+#include <FirebaseESP8266.h>
+
+void setup()
+{
+  Serial.begin(115200);
+
+  /*
+    WiFiManager
+  */
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+  //reset saved settings
+  // wifiManager.resetSettings();
+
+  //set custom ip for portal
+  //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+  //fetches ssid and pass from eeprom and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "bcn+"
+  //and goes into a blocking loop awaiting configuration
+  wifiManager.autoConnect("bcn+");
+
+  //if you get here you have connected to the WiFi
+  Serial.println("Device Connected! :P");
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
